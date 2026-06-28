d508518c26f8464ad9a3adaf4c4c76b12fd3b2b2
Lucas Silva <lucas1604.ti@gmail.com>
2026-06-28 18:26:19 -0300

    Aprendi a alterar valores de variáveis

---
+++ JavaScript/5 Alterando valores de variáveis/index.js
@@ -0,0 +1,17 @@
+// NULL
+/* Representa a ausência intencional de qualquer valor */
+var teste = null;
+
+// Undefined
+/* Representa a ausência de valor (não inicializado) */
+var teste2;
+
+console.log(teste);
+
+var teste = 'Teste realizado com sucesso';
+
+console.log(teste);
+
+var teste = 127.75;
+
+console.log(teste);
