e96dc100067c639cd2f933005786caea0a519a5e
Lucas Silva <lucas1604.ti@gmail.com>
2026-06-29 13:16:43 -0300

    Aprendi operadores de comparação

---
+++ JavaScript/7 Operadores condicionais/index.js
@@ -0,0 +1,9 @@
+
+
+if (4 !== 4) {
+    document.write('Entrou dentro do bloco if (verdadeiro) com sucesso. Lá ele...');
+}
+
+else {
+    document.write('Entrou dentro do bloco else (falso) com sucesso');
+}
