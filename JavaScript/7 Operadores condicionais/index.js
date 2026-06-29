11155a98ef7817adea396cbcf21724d984136dfa
Lucas Silva <lucas1604.ti@gmail.com>
2026-06-29 13:36:42 -0300

    Pratiquei mais sobre operadores de comparação

--- JavaScript/7 Operadores condicionais/index.js
+++ JavaScript/7 Operadores condicionais/index.js
@@ -1,9 +1,18 @@
+var media = 7;
+var nota = prompt('Digite a nota do aluno:');
 
+if (nota < media) {
+    document.write('O aluno está abaixo da média');
+}
 
-if (4 !== 4) {
-    document.write('Entrou dentro do bloco if (verdadeiro) com sucesso. Lá ele...');
+else if (nota == media) {
+    document.write('O aluno está na média');
+}
+
+else if (nota >= '11') {
+    document.write('Mesmo o 11 sendo uma string, o interpretador JavaScript consegue entendê-lo como um valor numérico, fazendo assim com que os number consigam ser comparados com números mesmo do tipo string.');
 }
 
 else {
-    document.write('Entrou dentro do bloco else (falso) com sucesso');
+    document.write('O aluno está acima da média');
 }
