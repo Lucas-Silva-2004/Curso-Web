da49d25739e26cc6a5aa77b071dba601ebb407be
Lucas Silva <lucas1604.ti@gmail.com>
2026-06-30 12:51:10 -0300

    atualizei a pasta 'concatenação' para a sintaxe moderna

--- JavaScript/3_concatenacao/index.js
+++ JavaScript/3_concatenacao/index.js
@@ -1,14 +1,15 @@
-var nome = prompt('Digite o seu nome: ');
-var idade = prompt('Digite a sua idade: ');
-var idade2 = 50;
+let nome = prompt('Digite o seu nome: ');
+let idade = prompt('Digite a sua idade: ');
+let idade2 = 50;
 /* O prompt funciona como o "scanf" do C. Ou seja, ele permite a entrada de dados pelo usuário. */
+
+const mensagem = (nome, idade) => `Olá, ${nome}! Você tem ${idade} anos.`;
 
 console.log(idade);
 console.log(idade2);
 /* OBS: O prompt sempre retorna uma string, mesmo que o usuário digite um número. */
 
-document.write('<h1>Olá, ' + nome + '! Seja bem-vinda ao curso de JavaScript! Você tem ' + idade + ' anos.</h1>');
-/* o sinal de + é usado para concatenar strings. Também devemos fechar as aspas antes e abrir depois para separar as strings da variável*/
-/* Também deve ter cuidado com o contexto, pois o sinal de + também é utilizado para somar números */
+document.getElementById("texto").textContent = mensagem(nome, idade);
+/* Esta linha está recuperando o <p id="texto"> e está usando o .textContent para exibir a mensagem com as infos nome e idade */
 
 console.log("Deu tudo certo, patrão!");
