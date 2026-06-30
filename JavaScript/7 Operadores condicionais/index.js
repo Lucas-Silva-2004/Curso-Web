dfdfa31500c6137ba4321b6894f429e4bebc2f7d
Lucas Silva <lucas1604.ti@gmail.com>
2026-06-30 17:31:19 -0300

    atualizei 07 Operadores

--- JavaScript/7 Operadores condicionais/index.js
+++ JavaScript/7 Operadores condicionais/index.js
@@ -1,18 +1,22 @@
-var media = 7;
-var nota = prompt('Digite a nota do aluno:');
+let media = 7;
+let nota = prompt('Digite a nota do aluno:');
+
+const mensagem = document.getElementById("nota-Aluno");
 
 if (nota < media) {
-    document.write('O aluno está abaixo da média');
+    mensagem.innerHTML = `
+    O aluno está abaixo da média.
+    `
 }
 
 else if (nota == media) {
-    document.write('O aluno está na média');
-}
-
-else if (nota >= '11') {
-    document.write('Mesmo o 11 sendo uma string, o interpretador JavaScript consegue entendê-lo como um valor numérico, fazendo assim com que os number consigam ser comparados com números mesmo do tipo string.');
+    mensagem.innerHTML = `
+    O aluno está na média.
+    `
 }
 
 else {
-    document.write('O aluno está acima da média');
+    mensagem.innerHTML = `
+    O aluno está acima da média
+    `
 }
