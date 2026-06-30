277579dfa2d88a1a56c4ac9394b5067cc5fd958e
Lucas Silva <lucas1604.ti@gmail.com>
2026-06-30 14:13:50 -0300

    modernizei desafio ABC

--- JavaScript/6 Desafio A B C/index.js
+++ JavaScript/6 Desafio A B C/index.js
@@ -1,30 +1,32 @@
-var a = 10;
-var b = 20;
-var c = null;
+let a = 10;
+let b = 20;
+let c = null;
 
-document.write('A: ' + a + '<br />')
-document.write('B: ' + b + '<br />')
-document.write('C: ' + c + '<br />')
-
-document.write('<hr />')
+document.getElementById('antes-inversao').innerHTML = `
+    A: ${a}<br />
+    B: ${b}<br />
+    C: ${c}<br />
+`
+/* O .innerHTML pega todo o conteúdo do elemento selecionado, incluindo as tags. Diferente do .textContent que ignora as tags e escreve literal as tags. */
 
 // Lógica de inversão de valores das variáveis
-var c = b;
+c = b;
 console.log('O valor de B foi atribuído ao C');
 
-var b = a;
+b = a;
 console.log('O valor de A foi atribuído ao B');
 
-var a = c;
+a = c;
 console.log('O valor de C foi atribuído ao A');
 
-var c = null;
+c = null;
 console.log('O valor null voltou ao C');
 
 console.log('Os valores foram invertidos com sucesso');
 
-// Exibição dos valores alterados
 
-document.write('A: ' + a + '<br />');
-document.write('B: ' + b + '<br />');
-document.write('C: ' + c + '<br />');
+document.getElementById('depois-inversao').innerHTML = `
+    A: ${a}<br />
+    B: ${b}<br />
+    C: ${c}<br />
+`
