73fa2c4afdaf055f0bfcbddfdcd9efa0eba5a47f
Lucas Silva <lucas1604.ti@gmail.com>
2026-06-30 17:55:15 -0300

    fatorei 09 operadores logicos

--- JavaScript/9 Operadores lógicos/index.js
+++ JavaScript/9 Operadores lógicos/index.js
@@ -1,3 +1,11 @@
-if (2 == 2 && 'a' == 'b') {
-    document.write()
+const texto = document.getElementById('texto');
+
+if (2 == 2 && 'b' == 'a') {
+    texto.innerHTML = `
+    Está correto.
+    `
+} else {
+    texto.innerHTML = `
+    Não está correto
+    `
 }
