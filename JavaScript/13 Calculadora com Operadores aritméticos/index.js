b1e486a2b31bab7994fbdb2ce615f7ebff8209b0
Lucas Silva <lucas1604.ti@gmail.com>
2026-07-02 17:52:52 -0300

    melhorei calculadora

--- JavaScript/13 Calculadora com Operadores aritméticos/index.js
+++ JavaScript/13 Calculadora com Operadores aritméticos/index.js
@@ -7,6 +7,7 @@ let resultado = document.getElementById("resultado");
 /* 1. Validação: isNaN checa se a conversão para número falhou. */
 if (isNaN(num1) || isNaN(num2) || !operador) {
     alert("Por favor, digite um caractere ou número válido.");
+
 } else {
     /* As contas só serão feitas se os dados forem válidos. */
     let soma = (num1 + num2);
@@ -14,11 +15,27 @@ if (isNaN(num1) || isNaN(num2) || !operador) {
     let multiplicacao = (num1 * num2);
     let divisao = (num1 / num2);
     let resto = (num1 % num2);
-}
 
-switch (operador) {
-    case '+' :
-        resultado.innerHTML = `A soma entre ${num1} e ${num2} é ${soma}`;
-        console.log("soma bem sucedida.");
-        break;
+    switch (operador) {
+        case '+':
+            resultado.innerHTML = `A soma entre ${num1} e ${num2} é ${soma}`;
+            console.log("soma bem sucedida.");
+            break;
+
+        case '-':
+            resultado.innerHTML = `A subtração entre ${num1} e ${num2} é ${subtracao}`;
+            console.log("subtração bem sucedida.");
+            break;
+
+        case '*':
+            resultado.innerHTML = `A multiplicação entre ${num1} e ${num2} é ${multiplicacao}`;
+            console.log("multiplicação bem sucedida.");
+            break;
+
+        case '/':
+            divisao = parseInt(divisao);
+            resultado.innerHTML = `A divisão entre ${num1} e ${num2} é ${divisao}<br>Resto: ${resto}`;
+            console.log("divisão bem sucedida.");
+            break;
+    }
 }
