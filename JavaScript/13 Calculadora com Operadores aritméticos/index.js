77502a2e95c8dbb9f4af1c961f56a7d8295886d6
Lucas Silva <lucas1604.ti@gmail.com>
2026-07-02 16:55:01 -0300

    fiz funcao adicao

--- JavaScript/13 Calculadora com Operadores aritméticos/index.js
+++ JavaScript/13 Calculadora com Operadores aritméticos/index.js
@@ -1,27 +1,13 @@
-let num1 = 10;
-let num2 = 8;
+let num1 = parseInt(prompt("Digite um número: "));
+let num2 = parseInt(prompt("Digite outro número: "));
+let operador = prompt("Agora digite o operador desejado [+], [-], [*], [%], [/]");
 
-let soma = num1 + num2;
-let subtracao = num1 - num2;
-let multiplicacao = num1 * num2;
-let divisao = num1 / num2;
-divisao = parseInt(divisao);
+let resultado = document.getElementById("resultado");
 
-let resto = num1 % num2;
-resto = parseFloat(resto.toFixed(2));
-
-
-let textoSoma = document.getElementById("textoSoma");
-textoSoma.innerHTML = (`<p>A soma entre ${num1} e ${num2} é: ${soma}</p><br>`);
-
-let textoSubtracao = document.getElementById("textoSubtracao");
-textoSubtracao.innerHTML = (`<p>A subtracao entre ${num1} e ${num2} é: ${subtracao}</p><br>`);
-
-let textoMultiplicacao = document.getElementById("textoMultiplicacao");
-textoMultiplicacao.innerHTML = (`<p>A multiplicacao entre ${num1} e ${num2} é: ${multiplicacao}</p><br>`);
-
-let textoDivisao = document.getElementById("textoDivisao");
-textoDivisao.innerHTML = (`<p>A divisao entre ${num1} e ${num2} é: ${divisao}</p><br>`);
-
-let textoResto = document.getElementById("textoResto");
-textoResto.innerHTML = (`<p>O resto da divisao entre ${num1} e ${num2} é: ${resto}</p>`);
+switch (operador) {
+    case '+' :
+        let soma = (num1 + num2);
+        resultado.innerHTML = `A soma entre ${num1} e ${num2} é ${soma}`;
+        console.log("soma bem sucedida.");
+        break;
+}
