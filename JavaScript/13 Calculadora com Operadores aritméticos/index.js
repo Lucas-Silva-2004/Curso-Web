34b422130fc8eca08127820491e1ce6d3b81a948
Lucas Silva <lucas1604.ti@gmail.com>
2026-07-02 17:43:26 -0300

    fiz variaveisOperadores

--- JavaScript/13 Calculadora com Operadores aritméticos/index.js
+++ JavaScript/13 Calculadora com Operadores aritméticos/index.js
@@ -1,17 +1,23 @@
 let num1 = parseInt(prompt("Digite um número: "));
 let num2 = parseInt(prompt("Digite outro número: "));
-let operador = prompt("Agora digite o operador desejado [+], [-], [*], [%], [/]");
+let operador = prompt("Agora digite o operador desejado [+], [-], [*], [/]");
 
 let resultado = document.getElementById("resultado");
 
 /* 1. Validação: isNaN checa se a conversão para número falhou. */
 if (isNaN(num1) || isNaN(num2) || !operador) {
     alert("Por favor, digite um caractere ou número válido.");
+} else {
+    /* As contas só serão feitas se os dados forem válidos. */
+    let soma = (num1 + num2);
+    let subtracao = (num1 - num2);
+    let multiplicacao = (num1 * num2);
+    let divisao = (num1 / num2);
+    let resto = (num1 % num2);
 }
 
 switch (operador) {
     case '+' :
-        let soma = (num1 + num2);
         resultado.innerHTML = `A soma entre ${num1} e ${num2} é ${soma}`;
         console.log("soma bem sucedida.");
         break;
