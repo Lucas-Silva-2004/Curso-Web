a83139af4ce2b82976ba1d07accd5dfddc49b6f8
Lucas Silva <lucas1604.ti@gmail.com>
2026-07-06 21:40:23 -0300

    completei calculadora imc

--- JavaScript/17 Exercício calc IMC/index.js
+++ JavaScript/17 Exercício calc IMC/index.js
@@ -5,9 +5,30 @@ altura = (altura / 100);
 let peso = prompt("Agora digite o seu peso: ");
 peso = parseFloat(peso);
 
+let M = (peso / (altura ** 2))
+/* Cálculo do IMC */
+
+let classificacao;
+
 resultado = document.getElementById("resultado");
-resultado.innerHTML = `
-    Nome: ${nome}<br>
-    Altura: ${altura}<br>
-    Peso: ${peso}
-`
+
+if (M > 16) {
+    classificacao = "Baixo peso, muito grave";
+} else if (M >= 16 && M <= 16.99) {
+    classificacao = "Baixo peso, grave";
+} else if (M >= 17 && M <= 18.49) {
+    classificacao = "Baixo peso";
+} else if (M >= 18.50 && M <= 24.99) {
+    classificacao = "Peso normal";
+} else if (M >= 25 && M <= 29.99) {
+    classificacao = "Sobrepeso";
+} else if (M >= 30 && M <= 34.99) {
+    classificacao = "Obesidade grau 1";
+} else if (M >= 35 && M <= 39.99) {
+    classificacao = "Obesidade grau 2";
+} else if (M >= 40) {
+    classificacao = "Obesidade grau 3";
+}
+
+resultado.innerHTML = `${nome} possui índice de massa corporal igual a ${M.toFixed(2)}, sendo classificado como: ${classificacao}`;
+/* O .toFixed(2) limita o número de casas decimais a 2 a ser exibido */
