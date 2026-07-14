eaf9c39285896bd4e16115f1d3c6d78ff88450b9
Lucas Silva <lucas1604.ti@gmail.com>
2026-07-13 23:01:03 -0300

    finalizei eventos formulario

---
+++ JavaScript/32 Eventos - formulário/index.js
@@ -0,0 +1,13 @@
+texto = document.getElementById('texto');
+
+// Eventos: janela:
+    // onfocus: dispara quando a janela recebe o foco do cursor do mouse ou do teclado.
+
+    // onblur: dispara quando a janela perde o foco do cursor do mouse ou do teclado.
+
+    // onchange: quando o usuário altera o valor de um campo de formulário e sai do campo (perde o foco). Ele é muito utilizado com o elemento select, mas também pode ser usado com input e textarea
+
+
+function acao() {
+    console.log("Evento disparado");
+}
