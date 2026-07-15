0ae95d79943e15977c07e62db71d40ec796199f8
Lucas Silva <lucas1604.ti@gmail.com>
2026-07-15 13:24:54 -0300

    aprendi manipular dom classes

---
+++ JavaScript/36 DOM - manipulando classes/index.js
@@ -0,0 +1,9 @@
+function modificarClasse1() {
+    document.getElementById("mensagem").className = 'estilo1';
+    console.log("estilo 1 aplicado.");  
+}
+
+function modificarClasse2() {
+    document.getElementById("mensagem").className = 'estilo2';
+    console.log("estilo 2 aplicado.");
+}
