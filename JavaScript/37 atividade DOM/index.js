b63c7842e4a6c1b34f49eabf83ba8664ee0edbfc
Lucas Silva <lucas1604.ti@gmail.com>
2026-07-15 19:09:58 -0300

    conclui atividade dom

--- JavaScript/37 atividade DOM/index.js
+++ JavaScript/37 atividade DOM/index.js
@@ -1,9 +1,24 @@
+// resgata o input
+let caixaTexto = document.getElementById("caixaTexto");
+
+// Mudar pra cor amarela ao entrar no foco
 function fundoAmarelo(corDeFundo) {
-    document.getElementById("mensagem").className = 'amarelo';
-    console.log("Fundo amarelo");
+    caixaTexto.className = 'amarelo'; /* define o atributo CSS da classe "amarelo" */
+    console.log("Fundo amarelo: entrou no foco");
 }
 
-function fundoTransparente() {
-    document.getElementById("mensagem").className = 'transparente';
-    console.log("Fundo transparente");
+// Sair do foco e o valor do input ser enviado
+function sairFoco() {
+    let texto = caixaTexto.value; /* resgata o valor do input */
+    console.log("Saiu do foco.");
+
+    if (texto.length < 3) {
+        // Se a quantidade de caracteres for inferior a três, altera a classe da caixa para a classe "vermelho"
+        caixaTexto.className = 'vermelho';
+        console.log("Fundo vermelho: valor menor que três.");
+    } else {
+        // Se a quantidade de caracteres for igual ou maior que três, altera a classe para a classe "verde"
+        caixaTexto.className = 'verde';
+        console.log("Fundo verde: valor igual ou maior que três.");
+    }
 }
