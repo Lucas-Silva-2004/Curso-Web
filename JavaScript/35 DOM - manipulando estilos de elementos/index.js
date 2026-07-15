9273a38ac4e9f551d6763284538fb1f0d84774ea
Lucas Silva <lucas1604.ti@gmail.com>
2026-07-15 12:50:32 -0300

    complementei elementos dom

--- JavaScript/35 DOM - manipulando estilos de elementos/index.js
+++ JavaScript/35 DOM - manipulando estilos de elementos/index.js
@@ -1,5 +1,9 @@
-function modificaEstilo(corDeFundo) {
+function modificaEstilo(corDeFundo, l, a) {
     document.getElementById("quadrado").style.backgroundColor = corDeFundo; /* acessar a propriedade "style", mais especificamente o background-color, e atribuir a cor de fundo que foi recebida por parâmetro. */
+
+    document.getElementById("quadrado").style.width = l; /* acessar a propriedade "width" e define como l (largura) */
+
+    document.getElementById("quadrado").style.height = a; /* acessar a propriedade "height" e define como a (altura) */
 }
 
 // Basicamente, cada botão possui o onClick com o atributo "modificaEstilo" e cada um com suas devidas cores. No JS, atribuimos uma função ao "ModificaEstilo" que recebe as cores como parâmetro "corDeFundo", resgata a id do quadrado e aplica a cor ao quadrado de acordo com o que foi definido em cada botão.
