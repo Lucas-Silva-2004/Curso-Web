b6546a3d1beb220713940f185de10f1696f543d8
Lucas Silva <lucas1604.ti@gmail.com>
2026-07-18 17:40:44 -0300

    finalizei appCalculadora

--- JavaScript/38 App calculadora/index.js
+++ JavaScript/38 App calculadora/index.js
@@ -1,10 +1,28 @@
 const calcular = (tipo, valor) => {
     if (tipo === 'acao') {
+        if (valor === 'c') {
+            // Limpar o visor (id tela)
+            document.getElementById("tela").value = ''
+        }
 
+        else if (valor === '+' || valor === '-' || valor === '*' || valor === '/' || valor === '.') {
+            // Concatenar as operações. Dessa forma eles não são substituídos ao digitar diferentes operações.
+            document.getElementById("tela").value += valor;
+        }
+
+        else if (valor === '=') {
+            let valorCampo = eval(document.getElementById("tela").value);
+            // O eval recebe uma string e tenta executá-lo como um código JS real. Muito útil para desenvolver calculadoras sem elaborar toda a lógica matemática. Neste caso, receberá todo o valor escrito na tela (o cálculo) como string e irá executá-lo como um cálculo real.
+            
+            document.getElementById("tela").value = valorCampo; 
+        }
     }
     
     else if (tipo === 'valor') {
-        let valorCampo = document.getElementById('tela').value /* O valor digitado foi atribuído à variável valorCampo */
-        document.getElementById('tela').value = valorCampo + valor; /* Selecionamos o input, acessamos o atributo value desse input e atribuimos o valor recebido pelo parâmetro + um novo valor. Dessa forma, é mostrada a sequência de números na tela ao invés de substituí-los cada vez que digita um novo número. */
+        let valorCampo = document.getElementById('tela').value += valor; // Concatena os valores digitados de modo que os números sejam adicionados sem substituir os anteriores
+    }
+
+    else {
+        tela.innerHTML = 'Erro desconhecido';
     }
 }
